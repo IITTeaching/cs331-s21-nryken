d4a42941804e6c6a65d45ee85a6bb96beb18ac2e
NoahRyken <66643170+NoahRyken@users.noreply.github.com>
2021-04-22 11:35:11 -0500

    Completed Project

--- project/radix-sort.py
+++ project/radix-sort.py
@@ -1,6 +1,9 @@
 from urllib import request
 
 def radix_a_book(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
+    return dictonaryRemover(bucket_all(textGet(), 0, longest(textGet())))
+
+def textGet(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
     words = []
     file = request.urlopen(book_url)
     for line in file:
@@ -25,33 +28,22 @@ def key_sorter(keys):
         sorted_keys.append(keys.pop(keys.index(val)))
     return sorted_keys
 
-
-def bucket_orderer(bucketed):
-    listed = []
-    for key in list(key_sorter(bucketed.keys())):
-        listed.append(bucketed[key])
-    return listed
-
-def list_level_sort(words, depth):
-    bucketed_words = char_bucketer(words, depth)
-    ordered = bucket_orderer(bucketed_words)
-    return ordered
-
-def sort_all(words='', depth=0, longer=0):
+def bucket_all(words='', depth=0, longer=0):
     out = char_bucketer(words, depth)
     depth += 1
     if depth < longer:
         for key in list(key_sorter(out.keys())):
-            out[key] = sort_all(out[key], depth, longer)
+            if key != '':
+                out[key] = bucket_all(out[key], depth, longer)
     return out
 
-def unDictionarier(theInput):
+def dictonaryRemover(theInput):
     out = []
     if type(theInput) == list:
         return theInput
     else:
         for key in list(key_sorter(theInput)):
-            out += unDictionarier(theInput[key])
+            out += dictonaryRemover(theInput[key])
         return out
    
 def longest(words):
@@ -61,12 +53,8 @@ def longest(words):
             longest = len(word)
     return longest
 
-
-def sort(words):
-    return unDictionarier(sort_all(radix_a_book(), 0, longest(words)))
-
 def testAndCompare(words):
-    mine = sort(words)
+    mine = radix_a_book(words)
     python = sorted(words)
     errors = 0
     for index in range(len(mine)):
@@ -74,5 +62,3 @@ def testAndCompare(words):
             errors += 1
             print("Failute at index " + str(index))
     print(errors)
-
-testAndCompare(radix_a_book())
