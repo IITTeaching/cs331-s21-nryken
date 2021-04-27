2b0a9648ad39d6f129599a7babc835a31b5c5426
NoahRyken <66643170+NoahRyken@users.noreply.github.com>
2021-04-26 20:47:25 -0500

    Updated Project

--- project/radix-sort.py
+++ project/radix-sort.py
@@ -1,7 +1,12 @@
 from urllib import request
 
+#w = maximum key length
+#n = number of elements
+#k = number of keys
+
 def radix_a_book(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
-    return dictonaryRemover(bucket_all(textGet(), 0, longest(textGet())))
+    text = textGet(book_url)
+    return dictonaryRemover(bucket_all(text, 0, longest(text)))
 
 def textGet(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
     words = []
@@ -11,7 +16,7 @@ def textGet(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
         words += decoded_line.split(' ')
     return words
 
-def char_bucketer(words, loc=0):
+def char_bucketer(words, loc=0): #n
     buckets = {}
     for word in words:
         try:
@@ -20,7 +25,7 @@ def char_bucketer(words, loc=0):
             buckets[word[loc:loc+1]] = [word]
     return buckets
 
-def key_sorter(keys):
+def key_sorter(keys): #k^2
     keys = list(keys)
     sorted_keys = []
     for _ in range(len(keys)):
@@ -28,7 +33,7 @@ def key_sorter(keys):
         sorted_keys.append(keys.pop(keys.index(val)))
     return sorted_keys
 
-def bucket_all(words='', depth=0, longer=0):
+def bucket_all(words='', depth=0, longer=0): #w*n
     out = char_bucketer(words, depth)
     depth += 1
     if depth < longer:
@@ -37,7 +42,7 @@ def bucket_all(words='', depth=0, longer=0):
                 out[key] = bucket_all(out[key], depth, longer)
     return out
 
-def dictonaryRemover(theInput):
+def dictonaryRemover(theInput): #n
     out = []
     if type(theInput) == list:
         return theInput
@@ -46,16 +51,16 @@ def dictonaryRemover(theInput):
             out += dictonaryRemover(theInput[key])
         return out
    
-def longest(words):
+def longest(words): #n
     longest = 0
     for word in words:
         if len(word) > longest:
             longest = len(word)
     return longest
 
-def testAndCompare(words):
-    mine = radix_a_book(words)
-    python = sorted(words)
+def testAndCompare(book_url='https://www.gutenberg.org/files/84/84-0.txt'): #Personal Test
+    mine = radix_a_book(book_url)
+    python = sorted(textGet(book_url))
     errors = 0
     for index in range(len(mine)):
         if mine[index] != python[index]:
